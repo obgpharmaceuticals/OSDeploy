2cf2123dc61d23964e0f76a36497dfaa9a6ef353
Danny Dunwoody <danny.dunwoody@obg.co.uk>
2022-11-10 21:26:35 +0000

    Create postdeploy.ps1

---
+++ postdeploy.ps1
@@ -0,0 +1,27 @@
+#=======================================================================
+#   PostOS: AutopilotOOBE Staging
+#=======================================================================
+$AutopilotOOBEJson = @'
+{
+    "Assign":  {
+                   "IsPresent":  true
+               },
+    "GroupTag":  "ProductivityDesktop",
+    "GroupTagOptions":  [
+                            "ProductivityDesktop",
+                            "ProductivityLaptop",
+                            "LineOfBusiness"
+                        ],
+    "Hidden":  [
+                   "AddToGroup",
+                   "AssignedComputerName",
+                   "AssignedUser",
+                   "PostAction"
+               ],
+    "PostAction":  "Quit",
+    "Run":  "NetworkingWireless",
+    "Docs":  "https://autopilotoobe.osdeploy.com/",
+    "Title":  "OBG Autopilot Registration"
+}
+'@
+$AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json"
