3aa0fb7300b5a4f7162de5adb9258437b18fd292
OBGPharma <79580065+OBGPharma@users.noreply.github.com>
2022-11-13 20:04:09 +0000

    Update OBGOSDeploy.ps1

--- OBGOSDeploy.ps1
+++ OBGOSDeploy.ps1
@@ -205,4 +205,44 @@ Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
 $OOBETasksCMD | Out-File -FilePath 'C:\Windows\System32\OOBE.CMD' -Encoding ascii -Force
 
+#=======================================================================
+# UnattendXml
+#=======================================================================
+$UnattendXml = @'
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="specialize">
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <RunSynchronous>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <Description>OSDCloud Specialize</Description>
+                    <Path>oobe.cmd</Path>
+                </RunSynchronousCommand>
+            </RunSynchronous>
+        </component>
+    </settings>
+</unattend>
+'@
+    #=======================================================================
+    # Directories
+    #=======================================================================
+    if (-NOT (Test-Path 'C:\Windows\Panther')) {
+        New-Item -Path 'C:\Windows\Panther'-ItemType Directory -Force -ErrorAction Stop | Out-Null
+    }
+    #=======================================================================
+    # Panther Unattend
+    #=======================================================================
+    $Panther = 'C:\Windows\Panther'
+    $UnattendPath = "$Panther\Unattend.xml"
+
+    Write-Verbose -Verbose "Setting $UnattendPath"
+    $UnattendXml | Out-File -FilePath $UnattendPath -Encoding utf8 -Force
+    #=======================================================================
+    # Use-WindowsUnattend
+    #=======================================================================
+    Write-Verbose -Verbose "Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath"
+    Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath -Verbose
+    #=======================================================================
+
 Restart-Computer
