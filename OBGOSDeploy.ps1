23cb76448d46ef508b12a32e44851e65870ecfcd
danny-dunwoody <79580065+danny-dunwoody@users.noreply.github.com>
2023-05-26 12:12:48 +0100

    Update OBGOSDeploy.ps1

--- OBGOSDeploy.ps1
+++ OBGOSDeploy.ps1
@@ -23,7 +23,7 @@ function Show-ImageMenu
     Write-Host "1: Local Windows 10 22H2"
     Write-Host "2: Cloud Windows 10 22H2"
     Write-Host "3: Local Windows 10 20H2"
-    Write-Host "4: Cloud Windows 10 20H2"
+    Write-Host "4: Cloud Windows 10 21H2"
     Write-Host "5: Cloud Windows 11 22H2"
 }
 
@@ -75,7 +75,7 @@ do
                 $ImageURL = "http://10.1.100.25/20h2.wim"
             } '4' {
                 $ImageLocation = "Cloud"
-                $OS = "Windows 10 20H2 x64"
+                $OS = "Windows 10 21H2 x64"
             } '5' {
                 $ImageLocation = "Cloud"
                 $OS = "Windows 11 22H2 x64"
