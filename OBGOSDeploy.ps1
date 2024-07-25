e7fa4ca05c9cfca876dda9520e4882e1b4430a97
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2024-07-25 08:28:19 +0100

    Update OBGOSDeploy.ps1

--- OBGOSDeploy.ps1
+++ OBGOSDeploy.ps1
@@ -157,11 +157,11 @@ $OOBEJson = @"
 }
 "@
 
-#If (!(Test-Path "C:\ProgramData\OSDeploy")) {
+If (!(Test-Path "C:\ProgramData\OSDeploy")) {
     
 New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
 
-#}
+}
 
 $OOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OOBE.json" -Encoding ascii -Force
 
