6a1b98a99bcf6b546f4319b3f03694a6bc97cece
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-05-27 14:06:19 +0100

    Update Start-OSDCloudNew.ps1

--- Start-OSDCloudNew.ps1
+++ Start-OSDCloudNew.ps1
@@ -41,7 +41,7 @@ $OSDCloudConfig = @{
 
 # Confirm action
 Write-Host "`nThis will wipe Disk 0 and deploy Windows 11 with Autopilot enrollment!" -ForegroundColor Yellow
-Read-Host "Press [ENTER] to continue..."
+# Read-Host "Press [ENTER] to continue..."
 
 # Wipe and partition Disk 0
 Write-Host "Wiping Disk 0..." -ForegroundColor Red
@@ -50,6 +50,6 @@ Initialize-Disk -Number 0 -PartitionStyle GPT
 New-Partition -DiskNumber 0 -UseMaximumSize -AssignDriveLetter | Format-Volume -FileSystem NTFS -NewFileSystemLabel "OSDisk" -Confirm:$false
 
 # Start the cloud deployment
-Read-Host "Ready to install, press a key"
+# Read-Host "Ready to install, press a key"
 Start-OSDCloud @OSDCloudConfig
-Read-Host "Finished, press a key" 
+# Read-Host "Finished, press a key" 
