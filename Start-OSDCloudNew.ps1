678dbb457b7f1cb60111d991c0c32b293b88a6ed
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-05-29 13:17:01 +0100

    Update Start-OSDCloudNew.ps1

--- Start-OSDCloudNew.ps1
+++ Start-OSDCloudNew.ps1
@@ -1,137 +1,101 @@
 Write-Host "Start Process"
-$ErrorActionPreference = 'Stop'
-Set-StrictMode -Version Latest
 
-#=======================================================================
-#   Prompt for Group Tag
-#=======================================================================
-function Show-GroupTagMenu {
-    param (
-        [string]$Title = 'Select Device Type'
-    )
-    Write-Host "================ $Title ================" -ForegroundColor Cyan
-    Write-Host "1: Productivity Desktop"
-    Write-Host "2: Productivity Laptop"
-    Write-Host "3: Line of Business"
-}
-
-$GroupTag = "NotSet"
-do {
-    Show-GroupTagMenu
-    $selection = Read-Host "Please make a selection"
-    switch ($selection) {
-        '1' { $GroupTag = "ProductivityDesktop" }
-        '2' { $GroupTag = "ProductivityLaptop" }
-        '3' { $GroupTag = "LineOfBusinessDesktop" }
+try {
+    # Prompt for Group Tag
+    function Show-GroupTagMenu {
+        param ([string]$Title = 'Computer Type')
+        Write-Host "================ $Title ================"
+        Write-Host "1: Productivity Desktop"
+        Write-Host "2: Productivity Laptop"
+        Write-Host "3: Line of Business"
     }
-} until ($GroupTag -ne "NotSet")
 
-#=======================================================================
-#   Get Latest Windows 11 Enterprise x64 Volume License (en-GB)
-#=======================================================================
-$OS = Get-OSDCloudOperatingSystem |
-    Where-Object {
-        $_.Name -like "Windows 11*" -and
-        $_.Edition -eq "Enterprise" -and
-        $_.Architecture -eq "x64" -and
-        $_.License -eq "Volume" -and
-        $_.Language -eq "en-gb"
-    } |
-    Sort-Object ReleaseId -Descending |
-    Select-Object -First 1
+    $GroupTag = "NotSet"
+    do {
+        Show-GroupTagMenu
+        $selection = Read-Host "Please make a selection"
+        switch ($selection) {
+            '1' { $GroupTag = "ProductivityDesktop" }
+            '2' { $GroupTag = "ProductivityLaptop" }
+            '3' { $GroupTag = "LineOfBusinessDesktop" }
+        }
+    } until ($GroupTag -ne "NotSet")
 
-if (-not $OS) {
-    Write-Error "Could not locate a suitable Windows 11 image. Check your internet connection and OSDCloud module."
-    exit 1
-}
-write-host "OS Pickup"
-#=======================================================================
-#   Define OSDCloud Parameters
-#=======================================================================
-$Params = @{
-    OSName     = $OS.Name
-    OSEdition  = $OS.Edition
-    OSLanguage = $OS.Language
-    OSLicense  = $OS.License
-    ZTI        = $true
-}
+    # Define latest Windows 11 image
+    $Params = @{
+        OSName     = "Windows 11 23H2 x64"
+        OSEdition  = "Enterprise"
+        OSLanguage = "en-gb"
+        OSLicense  = "Volume"
+        ZTI        = $true
+    }
 
-#=======================================================================
-#   Create OOBEW11 JSON File
-#=======================================================================
-$OOBEJson = @"
+    Write-Host "Starting OSDCloud deployment..."
+    Start-OSDCloud @Params
+
+    # Post-deployment OOBE setup
+    $OOBEJson = @"
 {
-    "Updates": [],
-    "RemoveAppx": [
-        "MicrosoftTeams",
-        "Microsoft.BingWeather",
-        "Microsoft.BingNews",
-        "Microsoft.GamingApp",
-        "Microsoft.GetHelp",
-        "Microsoft.Getstarted",
-        "Microsoft.Messaging",
-        "Microsoft.MicrosoftOfficeHub",
-        "Microsoft.MicrosoftSolitaireCollection",
-        "Microsoft.People",
-        "Microsoft.PowerAutomateDesktop",
-        "Microsoft.StorePurchaseApp",
-        "Microsoft.Todos",
-        "microsoft.windowscommunicationsapps",
-        "Microsoft.WindowsFeedbackHub",
-        "Microsoft.WindowsMaps",
-        "Microsoft.WindowsSoundRecorder",
-        "Microsoft.Xbox.TCUI",
-        "Microsoft.XboxGameOverlay",
-        "Microsoft.XboxGamingOverlay",
-        "Microsoft.XboxIdentityProvider",
-        "Microsoft.XboxSpeechToTextOverlay",
-        "Microsoft.YourPhone",
-        "Microsoft.ZuneMusic",
-        "Microsoft.ZuneVideo"
-    ],
-    "UpdateDrivers": true,
-    "UpdateWindows": true,
-    "AutopilotOOBE": true,
-    "GroupTagID": "$GroupTag"
+  "Updates": [],
+  "RemoveAppx": [
+    "MicrosoftTeams",
+    "Microsoft.BingWeather",
+    "Microsoft.BingNews",
+    "Microsoft.GamingApp",
+    "Microsoft.GetHelp",
+    "Microsoft.Getstarted",
+    "Microsoft.Messaging",
+    "Microsoft.MicrosoftOfficeHub",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.People",
+    "Microsoft.PowerAutomateDesktop",
+    "Microsoft.StorePurchaseApp",
+    "Microsoft.Todos",
+    "microsoft.windowscommunicationsapps",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsMaps",
+    "Microsoft.WindowsSoundRecorder",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo"
+  ],
+  "UpdateDrivers": true,
+  "UpdateWindows": true,
+  "AutopilotOOBE": true,
+  "GroupTagID": "$GroupTag"
 }
 "@
 
-$ProgramData = "C:\ProgramData\OSDeploy"
-New-Item -ItemType Directory -Path $ProgramData -Force | Out-Null
-$OOBEJson | Out-File "$ProgramData\OOBEW11.json" -Encoding ascii -Force
+    if (!(Test-Path "C:\ProgramData\OSDeploy")) {
+        New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force
+    }
+    $OOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OOBE.json" -Encoding ascii -Force
 
-#=======================================================================
-#   Create OOBEW11.CMD for Audit Mode Execution
-#=======================================================================
-$OOBETasksCMD = @"
+    $OOBETasksCMD = @"
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
+Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/obgpharmaceuticals/OSDeploy/main/OOBEW11.ps1
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/yourrepo/oobew11.ps1?$(Get-Random)
 "@
+    $OOBETasksCMD | Out-File -FilePath 'C:\Windows\System32\oobew11.cmd' -Encoding ascii -Force
 
-$OOBETasksCMD | Out-File 'C:\Windows\System32\OOBEW11.CMD' -Encoding ascii -Force
-
-#=======================================================================
-#   Unattend.xml to Trigger OOBE Script Post-Install
-#=======================================================================
-$UnattendXml = @'
+    # Configure Unattend.xml to run oobew11.cmd
+    $UnattendXml = @'
 <?xml version="1.0" encoding="utf-8"?>
 <unattend xmlns="urn:schemas-microsoft-com:unattend">
-    <settings pass="oobeSystem">
-        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
-            <Reseal>
-                <Mode>Audit</Mode>
-            </Reseal>
-        </component>
-    </settings>
     <settings pass="auditUser">
-        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <RunSynchronous>
                 <RunSynchronousCommand wcm:action="add">
                     <Order>1</Order>
-                    <Description>Start OOBEW11</Description>
-                    <Path>OOBEW11.CMD</Path>
-                    <WillReboot>Always</WillReboot>
+                    <Description>OOBE</Description>
+                    <Path>oobew11.cmd</Path>
+                    <WillReboot>Never</WillReboot>
                 </RunSynchronousCommand>
             </RunSynchronous>
             <Reseal>
@@ -142,24 +106,21 @@ $UnattendXml = @'
 </unattend>
 '@
 
-$Panther = 'C:\Windows\Panther'
-New-Item -Path $Panther -ItemType Directory -Force | Out-Null
-$UnattendPath = "$Panther\Unattend.xml"
-$UnattendXml | Out-File -FilePath $UnattendPath -Encoding utf8 -Force
+    if (-not (Test-Path 'C:\Windows\Panther')) {
+        New-Item -Path 'C:\Windows\Panther' -ItemType Directory -Force | Out-Null
+    }
+    $UnattendPath = 'C:\Windows\Panther\Unattend.xml'
+    $UnattendXml | Out-File -FilePath $UnattendPath -Encoding utf8 -Force
+    Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath -Verbose
 
-#=======================================================================
-#   Apply Unattend File
-#=======================================================================
-Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath -Verbose
-
-#=======================================================================
-#   Prevent BIOS Updates During Deployment (Lenovo Example)
-#=======================================================================
-New-Item -Path "HKLM:\SOFTWARE\Policies\Lenovo\BIOS" -Force | Out-Null
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Lenovo\BIOS" -Name "DisableBIOSUpdate" -Value 1 -Type DWord
-
-#=======================================================================
-#   Start OSDCloud Deployment
-#=======================================================================
-Write-Host "`nStarting OSDCloud for Windows 11 x64 Enterprise..." -ForegroundColor Green
-Start-OSDCloud @Params
+    Write-Host "Deployment script completed."
+    Read-Host "Press Enter to finish"
+}
+catch {
+    $err = $_.Exception.Message
+    Write-Host -ForegroundColor Red "\nAn error occurred: $err"
+    $logPath = "X:\Temp\OSDCloud_Error.log"
+    New-Item -Path (Split-Path $logPath) -ItemType Directory -Force | Out-Null
+    "\n[$(Get-Date)] $err" | Out-File -FilePath $logPath -Append -Encoding utf8
+    Read-Host "\nPress Enter to view the error and continue"
+}
