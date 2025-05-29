f898abad9646ce707a08aed18bb278edd086e29a
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-05-29 10:41:36 +0100

    Update Start-OSDCloudNew.ps1

--- Start-OSDCloudNew.ps1
+++ Start-OSDCloudNew.ps1
@@ -1,38 +1,131 @@
-# Ensure OSD module is available and import it
-if (-not (Get-Module -ListAvailable -Name OSD)) {
-    Install-Module -Name OSD -Force -Scope CurrentUser
+#=======================================================================
+#   Selection: Choose the type of system which is being deployed
+#=======================================================================
+
+function Show-GroupTagMenu {
+    Write-Host "================ Select Computer Type ================"
+    Write-Host "1: Productivity Desktop"
+    Write-Host "2: Productivity Laptop"
+    Write-Host "3: Line of Business"
 }
-Import-Module OSD
 
-Clear-Host
-Write-Host "Starting OSDCloud Deployment..." -ForegroundColor Cyan
-
-# Prompt for device type and set Group Tag accordingly
-$DeviceType = Read-Host "Enter device type: (1) Productivity Desktop, (2) Productivity Laptop, (3) Line of Business"
-
-switch ($DeviceType) {
-    '1' { $GroupTag = 'ProductivityDesktop' }
-    '2' { $GroupTag = 'ProductivityLaptop' }
-    '3' { $GroupTag = 'LineOfBusinessDesktop' }
-    default {
-        Write-Host "Invalid selection. Defaulting to 'ProductivityDesktop'" -ForegroundColor Yellow
-        $GroupTag = 'ProductivityDesktop'
+$GroupTag = "NotSet"
+do {
+    Show-GroupTagMenu
+    $selection = Read-Host "Please make a selection"
+    switch ($selection) {
+        '1' { $GroupTag = "ProductivityDesktop" }
+        '2' { $GroupTag = "ProductivityLaptop" }
+        '3' { $GroupTag = "LineOfBusinessDesktop" }
+        default { Write-Host "Invalid selection. Try again." -ForegroundColor Red }
     }
+} until ($GroupTag -ne "NotSet")
+
+#=======================================================================
+#   OS: Set up the OSD parameters for Windows 11
+#=======================================================================
+
+$Params = @{
+    OSName     = "Windows 11 22H2 x64"
+    OSEdition  = "Enterprise"
+    OSLanguage = "en-gb"
+    OSLicense  = "Volume"
+    ZTI        = $true
 }
 
-Write-Host "Autopilot Group Tag set to: $GroupTag" -ForegroundColor Green
+Write-Host -ForegroundColor Cyan "Starting Windows 11 Cloud Deployment..."
+Start-OSDCloud @Params
 
-# Configure OSDCloud deployment settings for Azure AD Join
-$OSDCloudConfig = @{
-    OSName         = "Windows 11 23H2 x64"
-    OSEdition      = "Enterprise"
-    OSLanguage     = "en-us"
-    OSLicense      = "Volume"
-    ZtdGroupTag    = $GroupTag
+#=======================================================================
+#   PostOS: OOBE Configuration
+#=======================================================================
+
+$OOBEJson = @"
+{
+    "Updates": [],
+    "RemoveAppx": [
+        "MicrosoftTeams",
+        "Microsoft.BingWeather",
+        "Microsoft.BingNews",
+        "Microsoft.GamingApp",
+        "Microsoft.GetHelp",
+        "Microsoft.Getstarted",
+        "Microsoft.Messaging",
+        "Microsoft.MicrosoftOfficeHub",
+        "Microsoft.MicrosoftSolitaireCollection",
+        "Microsoft.People",
+        "Microsoft.PowerAutomateDesktop",
+        "Microsoft.StorePurchaseApp",
+        "Microsoft.Todos",
+        "microsoft.windowscommunicationsapps",
+        "Microsoft.WindowsFeedbackHub",
+        "Microsoft.WindowsMaps",
+        "Microsoft.WindowsSoundRecorder",
+        "Microsoft.Xbox.TCUI",
+        "Microsoft.XboxGameOverlay",
+        "Microsoft.XboxGamingOverlay",
+        "Microsoft.XboxIdentityProvider",
+        "Microsoft.XboxSpeechToTextOverlay",
+        "Microsoft.YourPhone",
+        "Microsoft.ZuneMusic",
+        "Microsoft.ZuneVideo"
+    ],
+    "UpdateDrivers": true,
+    "UpdateWindows": true,
+    "AutopilotOOBE": true,
+    "GroupTagID": "$GroupTag"
+}
+"@
+
+if (!(Test-Path "C:\ProgramData\OSDeploy")) {
+    New-Item -Path "C:\ProgramData\OSDeploy" -ItemType Directory -Force
 }
 
-# Start OSDCloud deployment
-Write-Host "Launching OSDCloud..." -ForegroundColor Cyan
-Start-OSDCloud @OSDCloudConfig
+$OOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OOBE.json" -Encoding ascii -Force
 
-Read-Host "`nDeployment complete. Press ENTER to exit"
+Write-Host -ForegroundColor Green "Creating OOBE.CMD..."
+$OOBETasksCMD = @"
+PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
+Set PATH=%PATH%;C:\Program Files\WindowsPowerShell\Scripts
+Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/obgpharmaceuticals/OSDeploy/main/OOBE.ps1
+"@
+$OOBETasksCMD | Out-File -FilePath 'C:\Windows\System32\OOBE.CMD' -Encoding ascii -Force
+
+#=======================================================================
+#   Configure Unattend.xml to trigger OOBE.cmd
+#=======================================================================
+
+$UnattendXml = @'
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="auditUser">
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <RunSynchronous>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <Description>OOBE</Description>
+                    <Path>oobe.cmd</Path>
+                    <WillReboot>Always</WillReboot>
+                </RunSynchronousCommand>
+            </RunSynchronous>
+            <Reseal>
+                <Mode>OOBE</Mode>
+            </Reseal>
+        </component>
+    </settings>
+</unattend>
+'@
+
+$Panther = 'C:\Windows\Panther'
+if (-Not (Test-Path $Panther)) {
+    New-Item -Path $Panther -ItemType Directory -Force
+}
+
+$UnattendPath = "$Panther\Unattend.xml"
+$UnattendXml | Out-File -FilePath $UnattendPath -Encoding utf8 -Force
+
+Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath -Verbose
+
+Write-Host "`nRebooting Now..."
+# Restart-Computer -Force -Verbose
