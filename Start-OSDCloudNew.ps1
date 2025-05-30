92a1a6b47b977777cf39e38983f390d86b01231f
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-05-30 12:35:57 +0100

    Update Start-OSDCloudNew.ps1

--- Start-OSDCloudNew.ps1
+++ Start-OSDCloudNew.ps1
@@ -25,8 +25,8 @@ New-PSDrive -Name "Z" -PSProvider FileSystem -Root "\\10.1.192.20\Win11_24H2" -C
 
 #==================== Start OSDCloud from Custom WIM ====================
 $Params = @{
-    OSName     = "Windows 11 Enterprise"
-    OSEdition  = "Enterprise"
+    OSName     = "Windows 11 Pro"
+    OSEdition  = "Pro"
     OSLanguage = "en-gb"
     OSLicense  = "Volume"
     ZTI        = $true
