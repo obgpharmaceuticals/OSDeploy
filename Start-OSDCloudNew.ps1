39606f03ba8116f3ca0e968384fe2f715993d1ff
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-05-30 12:50:16 +0100

    Update Start-OSDCloudNew.ps1

--- Start-OSDCloudNew.ps1
+++ Start-OSDCloudNew.ps1
@@ -1,6 +1,9 @@
-Write-Host "Start Process New1"
+Write-Host "Start Process New"
 
-#==================== Computer Type Selection ====================
+#=======================================================================
+#   Selection: Choose the type of system which is being deployed
+#=======================================================================
+
 $GroupTag = "NotSet"
 do {
     Write-Host "================ Computer Type ================"
@@ -15,57 +18,59 @@ do {
     }
 } until ($GroupTag -ne "NotSet")
 
-#==================== Map Network Share ====================
-$SmbUser = "osduser"
-$SmbPassword = "osduser"  # Replace with actual password
-$SecurePass = ConvertTo-SecureString $SmbPassword -AsPlainText -Force
-$Cred = New-Object System.Management.Automation.PSCredential ($SmbUser, $SecurePass)
+#=======================================================================
+#   OS: Set up the OSD parameters for launch
+#=======================================================================
 
-New-PSDrive -Name "Z" -PSProvider FileSystem -Root "\\10.1.192.20\Win11_24H2" -Credential $Cred -Persist
-
-#==================== Start OSDCloud from Custom WIM ====================
 $Params = @{
-    OSName     = "Windows 11 Pro"
-    OSEdition  = "Professional"
+    OSName     = "Windows 11 23H2 x64"
+    OSEdition  = "Enterprise"
     OSLanguage = "en-gb"
     OSLicense  = "Volume"
     ZTI        = $true
 }
 
+#=======================================================================
+#  OS: Start-OSDCloud
+#=======================================================================
 Write-Host "Starting OSD Cloud"
 Start-OSDCloud @Params
 
-#==================== PostOS: OOBE Staging ====================
+#=======================================================================
+#   PostOS: OOBE Staging
+#=======================================================================
+
 $OOBEJson = @"
 {
-    "Updates":     [],
+    "Updates":     [
+                   ],
     "RemoveAppx":  [
-        "MicrosoftTeams",
-        "Microsoft.BingWeather",
-        "Microsoft.BingNews",
-        "Microsoft.GamingApp",
-        "Microsoft.GetHelp",
-        "Microsoft.Getstarted",
-        "Microsoft.Messaging",
-        "Microsoft.MicrosoftOfficeHub",
-        "Microsoft.MicrosoftSolitaireCollection",
-        "Microsoft.People",
-        "Microsoft.PowerAutomateDesktop",
-        "Microsoft.StorePurchaseApp",
-        "Microsoft.Todos",
-        "microsoft.windowscommunicationsapps",
-        "Microsoft.WindowsFeedbackHub",
-        "Microsoft.WindowsMaps",
-        "Microsoft.WindowsSoundRecorder",
-        "Microsoft.Xbox.TCUI",
-        "Microsoft.XboxGameOverlay",
-        "Microsoft.XboxGamingOverlay",
-        "Microsoft.XboxIdentityProvider",
-        "Microsoft.XboxSpeechToTextOverlay",
-        "Microsoft.YourPhone",
-        "Microsoft.ZuneMusic",
-        "Microsoft.ZuneVideo"
-    ],
+                    "MicrosoftTeams",
+                    "Microsoft.BingWeather",
+                    "Microsoft.BingNews",
+                    "Microsoft.GamingApp",
+                    "Microsoft.GetHelp",
+                    "Microsoft.Getstarted",
+                    "Microsoft.Messaging",
+                    "Microsoft.MicrosoftOfficeHub",
+                    "Microsoft.MicrosoftSolitaireCollection",
+                    "Microsoft.People",
+                    "Microsoft.PowerAutomateDesktop",
+                    "Microsoft.StorePurchaseApp",
+                    "Microsoft.Todos",
+                    "microsoft.windowscommunicationsapps",
+                    "Microsoft.WindowsFeedbackHub",
+                    "Microsoft.WindowsMaps",
+                    "Microsoft.WindowsSoundRecorder",
+                    "Microsoft.Xbox.TCUI",
+                    "Microsoft.XboxGameOverlay",
+                    "Microsoft.XboxGamingOverlay",
+                    "Microsoft.XboxIdentityProvider",
+                    "Microsoft.XboxSpeechToTextOverlay",
+                    "Microsoft.YourPhone",
+                    "Microsoft.ZuneMusic",
+                    "Microsoft.ZuneVideo"
+                   ],
     "UpdateDrivers": true,
     "UpdateWindows": true,
     "AutopilotOOBE": true,
@@ -76,6 +81,7 @@ $OOBEJson = @"
 If (!(Test-Path "C:\ProgramData\OSDeploy")) {
     New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force
 }
+
 $OOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OOBE.json" -Encoding ascii -Force
 
 Write-Host -ForegroundColor Green "Create C:\Windows\System32\oobew11.cmd"
@@ -85,9 +91,12 @@ Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/obgpharmaceuticals/OSDeploy/main/oobew11.ps1
 "@
+
 $OOBETasksCMD | Out-File -FilePath 'C:\Windows\System32\oobew11.cmd' -Encoding ascii -Force
 
-#==================== Unattend XML ====================
+#=======================================================================
+# UnattendXml
+#=======================================================================
 $UnattendXml = @'
 <?xml version="1.0" encoding="utf-8"?>
 <unattend xmlns="urn:schemas-microsoft-com:unattend">
@@ -102,10 +111,10 @@ $UnattendXml = @'
         <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <RunSynchronous>
                 <RunSynchronousCommand wcm:action="add">
-                    <Order>1</Order>
-                    <Description>OOBE</Description>
-                    <Path>oobew11.cmd</Path>
-                    <WillReboot>Always</WillReboot>
+                <Order>1</Order>
+                <Description>OOBE</Description>
+                <Path>oobew11.cmd</Path>
+                <WillReboot>Always</WillReboot>
                 </RunSynchronousCommand>
             </RunSynchronous>
             <Reseal>
@@ -116,12 +125,17 @@ $UnattendXml = @'
 </unattend>
 '@
 
-if (-not (Test-Path 'C:\Windows\Panther')) {
-    New-Item -Path 'C:\Windows\Panther' -ItemType Directory -Force | Out-Null
+if (-NOT (Test-Path 'C:\Windows\Panther')) {
+    New-Item -Path 'C:\Windows\Panther'-ItemType Directory -Force -ErrorAction Stop | Out-Null
 }
-$UnattendPath = "C:\Windows\Panther\Unattend.xml"
+
+$Panther = 'C:\Windows\Panther'
+$UnattendPath = "$Panther\Unattend.xml"
+
+Write-Verbose -Verbose "Setting $UnattendPath"
 $UnattendXml | Out-File -FilePath $UnattendPath -Encoding utf8 -Force
+Write-Verbose -Verbose "Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath"
 Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath -Verbose
 
-Write-Host "`nRebooting Now"
+Write-Host "\nRebooting Now"
 Write-Host "Restart-Computer -Force -Verbose"
