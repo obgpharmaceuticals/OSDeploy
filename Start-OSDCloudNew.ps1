8bddc739c5eae03198c514499bf31ff1eb8e37bf
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-06-30 10:51:56 +0100

    Update Start-OSDCloudNew.ps1

--- Start-OSDCloudNew.ps1
+++ Start-OSDCloudNew.ps1
@@ -214,7 +214,7 @@ exit /b 0
 
     Write-Host "Deployment script completed. Rebooting in 5 seconds..."
     Start-Sleep -Seconds 5
-    Restart-Computer -Force
+    # Restart-Computer -Force
 }
 catch {
     Write-Error "Deployment failed: $_"
