c2a41132b0c3f02ad0bfd994250e1b96a3307590
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-08-05 16:23:57 +0100

    Update Start-OSDCloudNew.ps1

--- Start-OSDCloudNew.ps1
+++ Start-OSDCloudNew.ps1
@@ -6,7 +6,6 @@ try {
 
     # Prompt for system type
     Write-Host "Select system type:"
-    Write-Host "1. Productivity Desktop"
     Write-Host "2. Productivity Laptop"
     Write-Host "3. Line of Business Desktop"
     $selection = Read-Host "Enter choice (1-3)"
@@ -87,7 +86,7 @@ try {
 
     if (-not (Test-Path "S:\EFI\Microsoft\Boot")) {
         Write-Host "Creating EFI folder structure..."
-        New-Item -Path "S:\EFI\Microsoft\Boot" -ItemType Directory -Force | Out-Null
+        New-Item -ItemType Directory -Force | Out-Null
     }
 
     Write-Host "Running bcdboot to create UEFI boot entry..."
@@ -129,6 +128,7 @@ try {
         CloudAssignedTenantId          = "c95ebf8f-ebb1-45ad-8ef4-463fa94051ee"
         CloudAssignedTenantDomain      = "obgpharma.onmicrosoft.com"
         DeviceType                     = $GroupTag
+        CloudAssignedInstallId         = 3
         EnableUserStatusTracking       = $true
         EnableUserConfirmation         = $true
         EnableProvisioningDiagnostics  = $true
@@ -196,11 +196,7 @@ echo Timestamp: %DATE% %TIME% >> %LOGFILE%
 
 timeout /t 10 > nul
 
-if exist "%SCRIPT%" (
-    powershell.exe -ExecutionPolicy Bypass -NoProfile -File "%SCRIPT%" -TenantId "c95ebf8f-ebb1-45ad-8ef4-463fa94051ee" -AppId "faa1bc75-81c7-4750-ac62-1e5ea3ac48c5" -AppSecret "ouu8Q~h2IxPhfb3GP~o2pQOvn2HSmBkOm2D8hcB-" -GroupTag "$GroupTag" -Online -Assign >> %LOGFILE% 2>&1
-) else (
-    echo ERROR: Script not found at %SCRIPT% >> %LOGFILE%
-)
+powershell.exe -ExecutionPolicy Bypass -NoProfile -ImportSystemModules -ModulePath "%MODULE_PATH%" -File "%SCRIPT%" -TenantId "c95ebf8f-ebb1-45ad-8ef4-463fa94051ee" -AppId "faa1bc75-81c7-4750-ac62-1e5ea3ac48c5" -AppSecret "ouu8Q~h2IxPhfb3GP~o2pQOvn2HSmBkOm2D8hcB-" -GroupTag "$GroupTag" -Online -Assign >> %LOGFILE% 2>&1
 
 echo Waiting 300 seconds (5 minutes) to ensure upload finishes and prevent reboot... >> %LOGFILE%
 timeout /t 300 /nobreak > nul
@@ -213,7 +209,7 @@ exit /b 0
     Write-Host "SetupComplete.cmd created successfully."
     Write-Host "Deployment script completed. Rebooting in 5 seconds..."
     Start-Sleep -Seconds 5
-    # Restart-Computer -Force
+    Restart-Computer -Force
 
 }
 catch {
