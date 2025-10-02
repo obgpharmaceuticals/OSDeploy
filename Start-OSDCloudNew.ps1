2859cd37ef5cda46de4481530accf32934c1bfef
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-10-02 08:21:56 +0100

    Update Start-OSDCloudNew.ps1

--- Start-OSDCloudNew.ps1
+++ Start-OSDCloudNew.ps1
@@ -205,7 +205,7 @@ echo Completed Autopilot upload + user assignment >> %LOGFILE%
 
     Write-Host "Deployment complete. Rebooting..."
     Start-Sleep -Seconds 5
-    # Restart-Computer -Force
+    Restart-Computer -Force
 
 } catch {
     Write-Error "Deployment failed: $_"
