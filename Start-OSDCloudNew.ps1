33d5c47701f5d3513205b82a391c9d5490d9444e
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-10-10 09:42:53 +0100

    Update Start-OSDCloudNew.ps1

--- Start-OSDCloudNew.ps1
+++ Start-OSDCloudNew.ps1
@@ -196,7 +196,6 @@ echo Completed Autopilot upload + user assignment >> %LOGFILE%
     New-Item -Path "HKLM:\SOFTWARE\OBG" -ErrorAction SilentlyContinue | Out-Null
     New-Item -Path "HKLM:\SOFTWARE\OBG\Signals" -ErrorAction SilentlyContinue | Out-Null
     New-ItemProperty -Path "HKLM:\SOFTWARE\OBG\Signals" -Name "ReadyForWin32" -PropertyType DWord -Value 1 -Force | Out-Null
-    Import-Module "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\OSDCloud\OSDCloud.psd1" -Force
     Save-FeatureUpdate
     Save-MyDriverPack
     Write-Host "Drivers a features updated. Rebooting in 5 seconds..."
