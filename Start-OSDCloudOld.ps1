991a0f0e2733bc4ab8de07721b2dc1eff6a466e6
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-10-13 09:27:46 +0100

    Update Start-OSDCloudOld.ps1

--- Start-OSDCloudOld.ps1
+++ Start-OSDCloudOld.ps1
@@ -126,7 +126,7 @@ powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Autopilot\Get-Window
 
     Write-Host "Deployment complete. Rebooting in 5 seconds..."
     Start-Sleep -Seconds 5
-    Restart-Computer -Force
+    # Restart-Computer -Force
 
 } catch {
     Write-Error "Deployment failed: $_"
