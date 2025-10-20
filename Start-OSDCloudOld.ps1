6a4605e8b8ff2b1abd66313e306640678cf077b6
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-10-20 09:17:21 +0100

    Update Start-OSDCloudOld.ps1

--- Start-OSDCloudOld.ps1
+++ Start-OSDCloudOld.ps1
@@ -172,11 +172,7 @@ powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Expand-StagedDriverP
 
 REM ADD DRIVERS TO DRIVERSTORE
 echo ==== ADD DRIVERS TO DRIVERSTORE ==== >> %LOGFILE%
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-"Get-ChildItem -Path 'C:\Drivers\sccm' -Recurse -Filter '*.inf' | ForEach-Object { ^
-    Write-Output ('Adding driver: ' + $_.FullName); ^
-    Start-Process pnputil.exe -ArgumentList '/add-driver', ('\"' + $_.FullName + '\"'), '/install' -Wait ^
-}" >> %LOGFILE% 2>&1
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-ChildItem -Path 'C:\Drivers\sccm' -Recurse -Filter '*.inf' | ForEach-Object { Write-Output ('Adding driver: ' + $_.FullName); Start-Process pnputil.exe -ArgumentList '/add-driver', ('\"' + $_.FullName + '\"'), '/install' -Wait }" >> %LOGFILE% 2>&1
 
 REM WINDOWS UPDATE
 echo ==== INSTALL WINDOWS UPDATES ==== >> %LOGFILE%
