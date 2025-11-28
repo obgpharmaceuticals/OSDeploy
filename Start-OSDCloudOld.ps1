3c190768238193350d0bcc758b5cf299b0a3bee9
jdowseobg <118835565+jdowseobg@users.noreply.github.com>
2025-11-28 10:46:46 +0000

    Update Start-OSDCloudOld.ps1

--- Start-OSDCloudOld.ps1
+++ Start-OSDCloudOld.ps1
@@ -193,7 +193,7 @@ echo Completed Autopilot upload + user assignment, driver expansion, DriverStore
 
     Write-Host "Drivers and features updated. Rebooting in 5 seconds..."
     Start-Sleep -Seconds 5
-    Restart-Computer -Force
+    # Restart-Computer -Force
 
 } catch {
     Write-Error "Deployment failed: $_"
